ca232a380ba0f1036a3faed56eac9b06734d6622
shixiaoyong <814740187@qq.com>
2020-11-01 21:57:38 +0800

    add project

---
+++ Chris.TechniqueTool/Chris.TechniqueTool.EntityGenerate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Chris.TechniqueTool.EntityGenerate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
